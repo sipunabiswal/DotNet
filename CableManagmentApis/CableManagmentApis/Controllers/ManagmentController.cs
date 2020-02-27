9dd9c418f27303bc9b4fb32219ff6eda19f13f96
DevilD <sipuna.biswal@gmail.com>
2020-02-28 02:24:16 +0530

    Cable Magement Apis created

---
+++ CableManagmentApis/CableManagmentApis/Controllers/ManagmentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CableManagmentApis.Models;
+
+namespace CableManagmentApis.Controllers
+{
+    public class ManagmentController : ApiController
+    {
+        public IHttpActionResult GetAllRoles()
+        {
+            IList<Roles> roles = null;
+            using (var cmd=new CableMagementEntities())
+            {
+                roles = cmd.User_Roles.Select(s=>new Roles() {
+                    Role_id=s.Role_Id,
+                    Role_Code=s.Role_Code,
+                    Role_Desc=s.Role_Desc
+                }).ToList<Roles>();
+            }
+            if (roles.Count==0)
+            {
+                return NotFound();
+            }
+            return Ok(roles);
+        }
+    }
+}
---
+++ CableManagmentApis/CableManagmentApis/Models/Roles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CableManagmentApis.Models
+{
+    public class Roles
+    {
+        public int Role_id { get; set; }
+        public string Role_Code { get; set; }
+        public string Role_Desc { get; set; }
+    }
+}
